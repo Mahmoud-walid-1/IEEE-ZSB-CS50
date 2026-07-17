0271afaaac9138290eb3b2b7c82cd3eeb25e01f0
Mahmoud Walid <mw7705052@gmail.com>
2026-07-17 21:47:31 +0300

    Initial commit

---
+++ task1/sortnumbers/sortnumbers.c
@@ -0,0 +1,89 @@
+#include<stdio.h>
+#include<cs50.h>
+
+int main(void){
+int A;
+int B;
+int C;
+    scanf("%i %i %i", &A ,&B ,&C );
+
+if(A>=B&&B>=C){
+ printf("%i\n",C);
+  printf("%i\n",B);
+   printf("%i\n",A);
+
+      printf("\n");
+
+printf("%i\n",A);
+  printf("%i\n",B);
+   printf("%i\n",C);
+
+}
+if(B>=A&&A>=C){
+ printf("%i\n",C);
+  printf("%i\n",A);
+   printf("%i\n",B);
+
+     printf("\n");
+
+printf("%i\n",A);
+  printf("%i\n",B);
+   printf("%i\n",C);
+}
+if(A>=C&&C>=B){
+ printf("%i\n",B);
+ printf("%i\n",C);
+ printf("%i\n",A);
+
+   printf("\n");
+
+printf("%i\n",A);
+  printf("%i\n",B);
+   printf("%i\n",C);
+}
+if(B>=C&&C>=A){
+ printf("%i\n",A);
+  printf("%i\n",C);
+   printf("%i\n",B);
+
+     printf("\n");
+
+printf("%i\n",A);
+  printf("%i\n",B);
+   printf("%i\n",C);
+}
+if(C>=A&&A>=B){
+printf("%i\n",B);
+  printf("%i\n",A);
+   printf("%i\n",C);
+
+   printf("\n");
+
+printf("%i\n",A);
+  printf("%i\n",B);
+   printf("%i\n",C);
+
+
+
+}
+if(C>=B&&B>=A){
+printf("%i\n",A);
+  printf("%i\n",B);
+   printf("%i\n",C);
+
+  printf("\n");
+
+printf("%i\n",A);
+  printf("%i\n",B);
+   printf("%i\n",C);
+
+}
+
+
+
+
+
+}
+
+
+
